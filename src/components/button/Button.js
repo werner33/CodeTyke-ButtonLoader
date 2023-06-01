e609b0bb8f37484912ff339326ac9543640407f8
EmaleeSoto <emaleesoto@gmail.com>
2023-06-01 12:41:26 -0400

    Added spinning logo and emoji with some styling to button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
+import loadingLogo from "../../assets/loading-gif.gif";
 
-import './Styles.scss';
+import "./Styles.scss";
 
 const Button = (props) => {
-
   return (
-    <div className={"submitButton " + (props.inactive ? "submitButton--inactive" : "")} onClick={props.handleSubmit} >
-      {props.label}
+    <div
+      id="submitAnswerButton"
+      className={
+        "submitButton " + (props.inactive ? "submitButton--inactive" : "")
+      }
+      onClick={props.handleSubmit}
+    >
+      <div>{props.leftIcon}</div>
+      <div>{props.label}</div>
+      <div>
+        <img src={props.rightIcon ? loadingLogo : " "} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Button;
--- src/components/learningModule/LearningModule.js
+++ src/components/learningModule/LearningModule.js
@@ -1,82 +1,99 @@
-import React from 'react';
-import ProgressBar from '../progressBar/ProgressBar';
-import SelectionBox from '../selectionBox/SelectionBox';
-import Button from '../button/Button';
-import Intro from '../intro/Intro';
+import React from "react";
+import ProgressBar from "../progressBar/ProgressBar";
+import SelectionBox from "../selectionBox/SelectionBox";
+import Button from "../button/Button";
+import Intro from "../intro/Intro";
 
-import './Styles.scss';
+import "./Styles.scss";
 
-const LearningModule = ({setGameStatus, gameStatus}) => {
+const LearningModule = ({ setGameStatus, gameStatus }) => {
   const [currentQuestionId, setCurrentQuestionId] = React.useState(0);
+  const [loading, setLoading] = React.useState(false);
   const [quizData, setQuizData] = React.useState({});
   const [isComplete, setIsComplete] = React.useState(false);
-  
-  let currentQuestion = quizData.questionArr ? quizData.questionArr[currentQuestionId]: {};
-  
-  React.useEffect(()=>{
-    getQuizData();
-  },[]);
 
-  const getQuizData=()=>{
+  let currentQuestion = quizData.questionArr
+    ? quizData.questionArr[currentQuestionId]
+    : {};
+
+  React.useEffect(() => {
+    getQuizData();
+  }, []);
+
+  const getQuizData = () => {
     fetch("http://localhost:8080/problems")
-      .then((res)=>{
+      .then((res) => {
         return res.json();
-      }).then((data)=>{
+      })
+      .then((data) => {
         setQuizData(data);
-      }).catch((err)=>{
+      })
+      .catch((err) => {
         console.log(err);
       });
-  }
+  };
 
-  const handleSubmit=()=> {
-      if(currentQuestionId < quizData.totalQuestions-1){
-        setTimeout(function(){
-          setCurrentQuestionId(currentQuestionId+1);
-        }, 700 );
+  const handleSubmit = () => {
+    setLoading(true);
+
+    if (currentQuestionId < quizData.totalQuestions - 1) {
+      setTimeout(function () {
+        setCurrentQuestionId(currentQuestionId + 1);
+        setLoading(false);
+      }, 700);
     } else if (!isComplete) {
       setIsComplete(true);
     } else {
       setCurrentQuestionId(0);
       setIsComplete(false);
-      setGameStatus('new');
+      setGameStatus("new");
     }
-  }
+  };
   let possibleAnswers = [];
-  if(currentQuestion.possibleAnswers){
+  if (currentQuestion.possibleAnswers) {
     possibleAnswers = currentQuestion.possibleAnswers.map((answer, index) => {
-      return <SelectionBox id={index} key={index} answer={answer} />
-    })
+      return <SelectionBox id={index} key={index} answer={answer} />;
+    });
   }
 
   return (
     <div className="learningModule">
-      { currentQuestion.title && !isComplete &&
+      {currentQuestion.title && !isComplete && (
         <>
-          <ProgressBar total={quizData.totalQuestions + 1} current={currentQuestion.id + 1} />
+          <ProgressBar
+            total={quizData.totalQuestions + 1}
+            current={currentQuestion.id + 1}
+          />
           <div className="learningModule__header">
-            <div className="learningModule__title">
-              { currentQuestion.title }
-            </div>
+            <div className="learningModule__title">{currentQuestion.title}</div>
             <div className="learningModule__subHeader">
-              { currentQuestion.additionalInfo }
+              {currentQuestion.additionalInfo}
             </div>
           </div>
 
           <div className="learningModule__answerArea">
-            <div className="learningModule__selections">
-              { possibleAnswers }
-            </div>
+            <div className="learningModule__selections">{possibleAnswers}</div>
             <div className="learningModule__submitButtonContainer">
-              <Button label="Submit" inactive handleSubmit={ handleSubmit } />
+              <Button
+                label="Submit"
+                leftIcon="👍"
+                rightIcon={loading}
+                inactive={loading ? true : false}
+                handleSubmit={handleSubmit}
+              />
             </div>
           </div>
         </>
-      }
-      {isComplete &&
-        <Intro message="Congratulations. You've completed this level!" buttonLabel="Play again"  buttonClick={handleSubmit} />
-      }
+      )}
+      {isComplete && (
+        <Intro
+          message="Congratulations. You've completed this level!"
+          buttonLabel="Play again"
+          buttonClick={handleSubmit}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default LearningModule;
